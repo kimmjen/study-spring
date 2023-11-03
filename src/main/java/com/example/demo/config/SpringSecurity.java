c253e55a944bffe62eff1ba05e91e2cc92ce55ba
kimmjen <50638199+kimmjen@users.noreply.github.com>
2023-11-04 01:05:39 +0900

    asd

---
+++ src/main/java/com/example/demo/config/SpringSecurity.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+//    @Autowired
+//    private
+}
